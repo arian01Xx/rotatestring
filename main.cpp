9bc74b41294c3c71c7edcc7917947aa5da1ee5a0
Arian Joseph <arian_joseph@outlook.es>
2024-11-02 23:49:25 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        return (s+s).find(goal)!=string::npos;
+    }
+};
+
+int main(){
+    return 0;
+}
